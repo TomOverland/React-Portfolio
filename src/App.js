2cc1e7bb071dab20df01a7c1a33abb503692a11a
Tom Overland <tomoverland03@gmail.com>
2020-11-27 16:41:24 -0600

    created App structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Header } from "../src/components/Header/Header";
+import Header from "../src/components/Header/Header";
+import Portfolio from "../src/components/Portfolio/Portfolio";
+import Projects from "../src/components/Project/Project";
+import Contact from "../src/components/Contact/Contact";
+import Footer from "../src/components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Route exact path="/" component={Portfolio} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
+      <Footer />
     </div>
   );
 }
