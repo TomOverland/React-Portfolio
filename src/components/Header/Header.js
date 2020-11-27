f250168764741f695def73b224c9afd13f3129d6
Tom Overland <tomoverland03@gmail.com>
2020-11-27 16:01:34 -0600

    created Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,2 +1,45 @@
 import React from "react";
 import "./Header.css";
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand">Tom Overland</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="./index.html">
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./portfolio.html">
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link disabled" href="./contact.html">
+              Contact <span className="sr-only">(current)</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
