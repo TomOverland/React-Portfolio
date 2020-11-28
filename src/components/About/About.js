1189b2fbfa04fb9b474bc13f3765cb8ab56f3275
Tom Overland <tomoverland03@gmail.com>
2020-11-28 17:46:05 -0600

    exported About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,48 +1,58 @@
-<div>
-  <div className="container mt-3">
-    <div className="row">
-      <div className="col d-flex justify-content-center">
-        <h1 className="white">Hi, I'm Tom.</h1>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col d-flex justify-content-center">
-        <h2 className="red">I design and build digital products.</h2>
-      </div>
-    </div>
-  </div>
+import React from "react";
+import "./About.css";
 
-  <div className="container">
-    <div className="row">
-      <div className="col-3"></div>
-      <div className="col-6">
-        <img
-          src="./assets/TomPhoto.JPG"
-          id="profile-photo"
-          className="img-fluid"
-          alt="Tom Overland"
-        />
+function About() {
+  return (
+    <div>
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col d-flex justify-content-center">
+            <h1 className="white">Hi, I'm Tom.</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col d-flex justify-content-center">
+            <h2 className="red">I design and build digital products.</h2>
+          </div>
+        </div>
       </div>
-      <div className="col-3"></div>
-    </div>
-  </div>
 
-  <div className="container biography mt-3">
-    <div className="row">
-      <div className="col d-flex justify-content-center">
-        <p className="white">
-          I am a Front-End Developer leveraging an emergency services background
-          to find creative solutions to modern day problems. I am completing a
-          certificate in Full Stack Development from University of Minnesota,
-          with newly developed skills in HTML, CSS, JavaScript, React.js and
-          responsive website design. I am known as a team player passionate
-          about developing applications, with a focus on mobile-first design and
-          development. With each project, my aim is to best engage my audience
-          for an impactful user experience. I’m excited to leverage my skills as
-          part of a fast-paced, quality-driven team to build better experiences
-          on the web.
-        </p>
+      <div className="container">
+        <div className="row">
+          <div className="col-3"></div>
+          <div className="col-6">
+            <img
+              src="./assets/TomPhoto.JPG"
+              id="profile-photo"
+              className="img-fluid"
+              alt="Tom Overland"
+            />
+          </div>
+          <div className="col-3"></div>
+        </div>
+      </div>
+
+      <div className="container biography mt-3">
+        <div className="row">
+          <div className="col d-flex justify-content-center">
+            <p className="white">
+              I am a Front-End Developer leveraging an emergency services
+              background to find creative solutions to modern day problems. I am
+              completing a certificate in Full Stack Development from University
+              of Minnesota, with newly developed skills in HTML, CSS,
+              JavaScript, React.js and responsive website design. I am known as
+              a team player passionate about developing applications, with a
+              focus on mobile-first design and development. With each project,
+              my aim is to best engage my audience for an impactful user
+              experience. I’m excited to leverage my skills as part of a
+              fast-paced, quality-driven team to build better experiences on the
+              web.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>;
+  );
+}
+
+export default About;
